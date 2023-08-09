a1601e602ae2149971d09bc0cfc7169077bbb504
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-08-08 22:56:52 -0300

    environtment variables config

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:cinemapedia/config/constants/environment.dart';
+
 class HomeScreen extends StatelessWidget {
 
     static const name = 'home-screen';
@@ -8,8 +10,10 @@ class HomeScreen extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
-        return const Scaffold(
-            body: Placeholder(),
+        return Scaffold(
+            body: Center(
+                child: Text(Environment.movieDbKey),
+            ),
         );
     }
 }
