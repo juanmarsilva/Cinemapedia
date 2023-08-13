58ed60b8fcd62344ca644bdaa111f8d762af0101
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-08-13 20:30:59 -0300

    create theme config

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,13 +1,45 @@
-
-
 import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+    Colors.blue,
+    Colors.teal,
+    Colors.green,
+    Colors.red,
+    Colors.purple,
+    Colors.deepPurple,
+    Colors.orange,
+    Colors.pink,
+    Colors.pinkAccent,
+];
 
 class AppTheme {
 
+    final int selectedColor;
+    final bool isDarkMode;
+
+    AppTheme({
+        this.selectedColor = 0,
+        this.isDarkMode = true,
+    }):
+        assert( selectedColor >= 0, 'Selected color must be grather than 0' ),
+        assert( selectedColor < colorList.length, 'Selected color must be less or equal than ${colorList.length - 1} ');
+
     ThemeData getTheme() => ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: const Color(0xFF2862F5),
-        brightness: Brightness.dark
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: colorList[ selectedColor ],
+        appBarTheme: const AppBarTheme(
+            centerTitle: true,
+        )
     );
 
+
+    AppTheme copyWith({
+        int? selectedColor,
+        bool? isDarkMode
+    }) => AppTheme(
+        selectedColor: selectedColor ?? this.selectedColor,
+        isDarkMode: isDarkMode ?? this.isDarkMode,
+    );
+    
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/providers/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -10,15 +11,18 @@ Future<void> main() async {
     runApp(const ProviderScope(child: MainApp()));
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
     const MainApp({super.key});
 
     @override
-    Widget build(BuildContext context) {
+    Widget build(BuildContext context, WidgetRef ref) {
+
+        final AppTheme appTheme = ref.watch( themeNotifierProvider );
+
         return MaterialApp.router(
             routerConfig: appRouter,
             debugShowCheckedModeBanner: false,
-            theme: AppTheme().getTheme(),
+            theme: AppTheme(selectedColor: appTheme.selectedColor, isDarkMode: appTheme.isDarkMode ).getTheme(),
         );
     }
 }
