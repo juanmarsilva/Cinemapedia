74bae360b61481dbd6db677177a3d07c1ba6be24
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-08-20 01:11:36 -0300

    get movie details by id

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -9,9 +9,18 @@ final appRouter = GoRouter(
         GoRoute(
             path: '/',
             name: HomeScreen.name,
-            builder: (context, state) => const HomeScreen()
+            builder: (context, state) => const HomeScreen(),
+            routes: [
+
+                GoRoute(
+                    path: 'movie/:id',
+                    name: MovieScreen.name,
+                    builder: (context, state) => MovieScreen( movieId: state.pathParameters['id'] ?? 'no-id' )
+                ),
+            ]
         ),
 
+       
 
 
     ]
---
+++ lib/presentation/providers/movies/movie_details_provider.dart
@@ -0,0 +1,31 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+final movieDetailProvider = StateNotifierProvider<MovieMapNotifier, Map<String, Movie>>((ref) => 
+    MovieMapNotifier(
+        getMovie: ref.watch( movieRepositoryProvider ).getMovieById
+    )
+);
+
+
+
+typedef GetMovieCallback = Future<Movie>Function(String movieId); 
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+
+    final GetMovieCallback getMovie;
+    
+    MovieMapNotifier({ required this.getMovie }): super({});
+
+    Future<void> loadMovie( String movieId ) async {
+        if( state[movieId] != null ) return;
+
+        final movie = await getMovie( movieId );
+
+        state = { ...state, movieId: movie };
+    }
+
+
+}
