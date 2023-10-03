9af658068ffe24f738abfcf6d109c58ca4c7a01a
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-10-03 00:22:55 -0300

    md create custom gradient widget

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -213,6 +213,13 @@ class _CustomSliverAppBar extends StatelessWidget {
             backgroundColor: Colors.black,
             expandedHeight: size.height * 0.7,
             foregroundColor: Colors.white,
+            actions: [
+                IconButton(
+                    onPressed: () {}, 
+                    // icon: const Icon(Icons.favorite_border_rounded)
+                    icon: const Icon(Icons.favorite_rounded, color: Colors.red)
+                )
+            ],
             flexibleSpace: FlexibleSpaceBar(
                 background: Stack(
                     children: [
@@ -228,42 +235,70 @@ class _CustomSliverAppBar extends StatelessWidget {
                             ),
                         ),
 
-                        const SizedBox.expand(
-                            child: DecoratedBox(
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                        begin: Alignment.topCenter,
-                                        end: Alignment.bottomCenter,
-                                        stops: [0.7, 1.0],
-                                        colors: [
-                                            Colors.transparent,
-                                            Colors.black87
-                                        ] 
-                                    )
-                                ),
-                            )
+                        const _CustomGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            stops: [0.7, 1.0],
+                            colors: [
+                                Colors.transparent,
+                                Colors.black87
+                            ],
                         ),
 
-                        const SizedBox.expand(
-                            child: DecoratedBox(
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                        begin: Alignment.topLeft,
-                                        stops: [0.0, 0.3],
-                                        colors: [
-                                            Colors.black87,
-                                            Colors.transparent,
-                                        ] 
-                                    )
-                                ),
-                            )
+                        const _CustomGradient(
+                            begin: Alignment.topRight,
+                            end: Alignment.bottomLeft,
+                            stops: [0.0, 0.2],
+                            colors: [
+                                Colors.black87,
+                                Colors.transparent,
+                            ],
                         ),
 
-
+                        const _CustomGradient(
+                            begin: Alignment.topLeft, 
+                            stops: [0.0, 0.3], 
+                            colors: [
+                                Colors.black87,
+                                Colors.transparent,
+                            ]
+                        )
                     ],
                 ),
             ),
 
+        );
+    }
+}
+
+class _CustomGradient extends StatelessWidget {
+
+    final AlignmentGeometry begin;
+    final AlignmentGeometry? end;
+    final List<double> stops;
+    final List<Color> colors;
+    
+
+    const _CustomGradient({
+        required this.begin, 
+        this.end, 
+        required this.stops, 
+        required this.colors
+    });
+
+    @override
+    Widget build(BuildContext context) {
+        return SizedBox.expand(
+            child: DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: begin,
+                        end: end ?? Alignment.center,
+                        stops: stops,
+                        colors: colors 
+                    )
+                ),
+            )
         );
     }
 }
