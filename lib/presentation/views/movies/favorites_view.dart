352ef6cc042f0b697bab4be9addbd5ef9244dd80
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-10-04 01:38:25 -0300

    renderice title of each movie on favorites

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:cinemapedia/presentation/presentation.dart';
+import 'package:cinemapedia/domain/domain.dart';
 
-
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
     const FavoritesView({super.key});
+    
+    @override
+    FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+    @override
+    void initState() {
+        super.initState();
+        ref.read( favoritesMoviesProvider.notifier ).loadNextPage();
+    }
 
     @override
     Widget build(BuildContext context) {
+
+        final bool initialLoading = ref.watch( initialLoadingProvider );
+
+        if( initialLoading ) return const FullScreenLoader();
+
+        final List<Movie> favoritesMovies = ref.watch( favoritesMoviesProvider ).values.toList();
+
         return Scaffold(
-            appBar: AppBar(
-                title: const Text('Favorites View')
-            ),
-            body: const Center(
-                child: Text('Favoritos'),
+            body: ListView.builder(
+                itemCount: favoritesMovies.length,
+                itemBuilder: (context, index) {
+
+                    final movie = favoritesMovies[index];
+
+                    return ListTile(
+                        title: Text( movie.title )
+                    );
+                }
             ),
         );
     }
